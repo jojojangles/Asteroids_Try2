e0fbd5eb26792c6365e3ec93021c8a59d1265a54
jojojangles <jcotosax@gmail.com>
2014-09-06 17:44:45 -0400

    Halfway through the tutorial

---
+++ Assets/Ship.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+	public Vector3 forceVector;
+	public float rotationSpeed;
+	public float rotation;
+
+	// Use this for initialization
+	void Start () {
+		forceVector.x = 1.0f;
+		rotationSpeed = 2.0f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetAxisRaw ("Vertical")>0){
+			gameObject.rigidbody.AddRelativeForce(forceVector);
+		} 
+		if(Input.GetAxisRaw ("Horizontal")>0){
+			rotation += rotationSpeed;
+			Quaternion rot = Quaternion.Euler (new Vector3(0,rotation,0));
+			gameObject.rigidbody.MoveRotation (rot);
+		} 
+		if(Input.GetAxisRaw ("Horizontal")<0){
+			rotation -= rotationSpeed;
+			Quaternion rot = Quaternion.Euler (new Vector3(0,rotation,0));
+			gameObject.rigidbody.MoveRotation (rot);
+		} 
+	}
+
+	void Update (){
+	}
+}
